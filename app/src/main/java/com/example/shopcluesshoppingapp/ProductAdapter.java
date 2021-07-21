76fc2b3b390765b4c2894591627c276757758d1a
Prabhakar <prabhakarkr802@gmail.com>
2021-07-21 16:28:02 +0530

    Category section layout complete

---
+++ app/src/main/java/com/example/shopcluesshoppingapp/ProductAdapter.java
@@ -0,0 +1,25 @@
+package com.example.shopcluesshoppingapp;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductViewHolder> {
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/shopcluesshoppingapp/ProductViewHolder.java
@@ -0,0 +1,13 @@
+package com.example.shopcluesshoppingapp;
+
+import android.view.View;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProductViewHolder extends RecyclerView.ViewHolder {
+    public ProductViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
