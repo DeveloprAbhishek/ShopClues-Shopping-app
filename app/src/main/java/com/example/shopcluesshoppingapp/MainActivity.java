92db93164d9bda1168f2f50f6f24b92db792004e
Anurag <anuragak07@gmail.com>
2021-07-21 21:17:02 +0530

    completed splashScreen also

--- app/src/main/java/com/example/shopcluesshoppingapp/MainActivity.java
+++ app/src/main/java/com/example/shopcluesshoppingapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.shopcluesshoppingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SystemClock.sleep(4000);
+        Intent LoginIntent =new Intent(MainActivity.this,LogInActivity.class);
+        startActivity(LoginIntent);
+        finish();
     }
 }
